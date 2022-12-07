09b96c9bfafb95c8aa04679c57f30c34ab5c68e7
VanshulB <vanshulbhatia@gmail.com>
2022-12-08 04:09:20 +0530

    Frontend till Home Page

---
+++ client/src/mutations/clientMutations.js
@@ -0,0 +1,25 @@
+import { gql } from "@apollo/client";
+
+const DELETE_CLENT = gql`
+  mutation deleteClient($id: ID!) {
+    deleteClient(id: $id) {
+      id
+      name
+      email
+      phone
+    }
+  }
+`;
+
+const ADD_CLIENT = gql`
+  mutation addClient($name: String!, $email: String!, $phone: String!) {
+    addClient(name: $name, email: $email, phone: $phone) {
+      id
+      name
+      email
+      phone
+    }
+  }
+`;
+
+export { DELETE_CLENT, ADD_CLIENT };
--- src/server/server.js
+++ src/server/server.js
@@ -3,11 +3,13 @@ require("dotenv").config();
 const colors = require("colors");
 const { graphqlHTTP } = require("express-graphql");
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 const schema = require("../server/schema/schema");
 const connectDB = require("../server/config/db");
 
 const app = express();
 connectDB();
+app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
